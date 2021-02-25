94f6605cfc85edc9677774479d2847f1384db1fb
LaZoark <emersonchen086@icloud.com>
2021-02-26 00:31:38 +0800

    first test

--- app/src/main/java/com/example/text_and_button_practice/MainActivity.java
+++ app/src/main/java/com/example/text_and_button_practice/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.text_and_button_practice;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
@@ -13,22 +15,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @SuppressLint("StaticFieldLeak")        //IDE教我這麼座以防止Memory leak
     static TextView txt;
     static float text_main_size;
-    public void button_bigger(View v){
+
+    public void button_bigger(View v) {  //這個View是必須的，傳物件用途
         txt = (TextView) findViewById(R.id.text_main);
         txt.setText("Hello, B0742006.");
-        text_main_size = txt.getTextSize();
-        text_main_size+=3;
+        text_main_size = txt.getTextSize() + 3;
         txt.setTextSize(TypedValue.COMPLEX_UNIT_PX, text_main_size);
-
     }
-    public void button_smaller(View v){
+
+    public void button_smaller(View v) {
         txt = (TextView) findViewById(R.id.text_main);
         txt.setText("Hello, B0742006.");
-        text_main_size = txt.getTextSize();
-        text_main_size-=3;
+        text_main_size = txt.getTextSize() - 3;
         txt.setTextSize(TypedValue.COMPLEX_UNIT_PX, text_main_size);
-
     }
 }
